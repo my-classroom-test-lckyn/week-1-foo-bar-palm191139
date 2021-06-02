1d170fc712a48869ade310da146b25c6049baf5f
Pawarit Thareechan <root@lckyn.com>
2021-06-03 00:28:17 +0700

    a

--- test_3.py
+++
@@ -1,6 +0,0 @@
-import sys
-import centuryFromYear
-
-year = int(input())
-
-print(centuryFromYear.centuryFromYear(year))
---
+++ test_centuryFromYear.py
@@ -0,0 +1,34 @@
+import sys
+import centuryFromYear
+
+year = int(input())
+
+# print("year : ", year)
+print((year))
+
+
+import centuryFromYear
+
+def test_centuryFromYear_1():
+    assert centuryFromYear.centuryFromYear(1) == 1
+
+def test_centuryFromYear_1():
+    assert centuryFromYear.centuryFromYear(100) == 1
+
+def test_centuryFromYear_1():
+    assert centuryFromYear.centuryFromYear(101) == 2
+
+def test_centuryFromYear_1():
+    assert centuryFromYear.centuryFromYear(1700) == 17
+
+def test_centuryFromYear_1():
+    assert centuryFromYear.centuryFromYear(1901) == 20
+
+def test_centuryFromYear_1():
+    assert centuryFromYear.centuryFromYear(2000) == 20
+
+def test_centuryFromYear_1():
+    assert centuryFromYear.centuryFromYear(1234) == 13
+
+def test_centuryFromYear_1():
+    assert centuryFromYear.centuryFromYear(500) == 5
